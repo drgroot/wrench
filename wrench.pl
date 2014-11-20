91e07006b65d2771549e33e6b87568b11c5528c0
Yusuf Ali <ali@yusuf.email>
2014-11-19 19:10:16 -0700

    working version of wrench pl

--- wrench.pl
+++ wrench.pl
@@ -17,7 +17,6 @@ while( my $file = readdir(DIR) ){
 	$srcds_list{$file} = 1;
 }
 closedir(DIR);
-#print Dumper(\%srcds_list);
 
 GetOptions(
 	 "start=s"	=> \&start
@@ -28,11 +27,14 @@ GetOptions(
 sub start{
 	my $srcds = $_[1];
 	if( $srcds eq "all" ){
-
+		foreach $srcds (keys %srcds_list){
+			next if isRunning($srcds);
+			makeStartScript($srcds);
+		}
 	}
 	else{
-
 		return unless exists ( $srcds_list{$srcds} );
+		return if isRunning($srcds);
 		makeStartScript($srcds);
 	}
 }
@@ -40,10 +42,28 @@ sub start{
 sub status{
 	my $srcds = $_[1];
 	return unless exists ( $srcds_list{$srcds} );
+	if(isRunning($srcds)){
+		print "Server: $srcds is already running\n";
+	}
+	else{
+		print "Server: $srcds is not running\n";	
+	}
 }
 
 sub list{
+	foreach my $srcds (keys %srcds_list){
+		status(0,$srcds);
+	}
+}
 
+sub isRunning{
+	my $srcds = $_[0];
+	my $screens = `screen -ls`;
+	if ($screens =~ m/\.$srcds\s+/){
+		return 1;
+	}
+
+	return 0;
 }
 
 sub makeStartScript{
